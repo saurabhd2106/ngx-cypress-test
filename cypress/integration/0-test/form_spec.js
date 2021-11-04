1004eb047ab3198639a0ae271f5fd309e11a95f5
Saurabh <saurabh.d2106@gmail.com>
2021-11-04 19:34:18 +0530

    PO

--- cypress/integration/0-test/form_spec.js
+++ cypress/integration/0-test/form_spec.js
@@ -3,7 +3,7 @@
 import { formPage } from "../../support/ngx-cypress-app/formsPage"
 import { navigateTo } from "../../support/ngx-cypress-app/navigationPage"
 
-describe("Form Test", ()=> {
+describe("Form Test", () => {
 
   beforeEach(() => {
 
@@ -15,46 +15,47 @@ describe("Form Test", ()=> {
 
   })
 
-  it("Using the Grid Test", ()=> {
+  it("Using the Grid Test", () => {
 
 
-     formPage.fillUsingTheGridForm("saurabh.d2106@fake.com", "4kjhsdh@dbg")
+    cy.screenshot()
+    formPage.fillUsingTheGridForm("saurabh.d2106@fake.com", "4kjhsdh@dbg")
 
 
   })
 
 
-  it("Using the Grid using Then method", ()=> {
+  it("Using the Grid using Then method", () => {
 
 
     // .as() -- allias
-    cy.contains("nb-card", "Using the Grid").then( function (usingTheGrid) {
+    cy.contains("nb-card", "Using the Grid").then(function (usingTheGrid) {
 
-      cy.wrap(usingTheGrid).find("[data-cy='inputEmail1']").type("Saurabh@fake.com")
+      cy.wrap(usingTheGrid).find("[data-cy='imputEmail1']").type("Saurabh@fake.com")
 
       cy.wrap(usingTheGrid).find("#inputPassword2").type("fhskdj@12")
 
       cy.wrap(usingTheGrid).find("button").click()
     })
 
-})
-
-it.only("Learn Invoke Method" , ()=> {
-
-  cy.get('[for="exampleInputEmail1"]').invoke("text").then( text => {
-
-    //Chai-jquery assertion
-    expect(text).to.equal("Email address")
-
-
   })
 
-  cy.get('[for="exampleInputEmail1"]').should("have.text", "Email address");
+  it("Learn Invoke Method", () => {
 
-  //Cypress assertion
-  cy.get("#exampleInputEmail1").invoke("attr", "placeholder").should("equal", "Email");
+    cy.get('[for="exampleInputEmail1"]').invoke("text").then(text => {
+
+      //Chai-jquery assertion
+      expect(text).to.equal("Email address")
+
+
+    })
+
+    cy.get('[for="exampleInputEmail1"]').should("have.text", "Email address");
+
+    //Cypress assertion
+    cy.get("#exampleInputEmail1").invoke("attr", "placeholder").should("equal", "Email");
+  })
+
+
+
 })
-
-
-
-}) 
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.Screenshot.defaults({
+  blackout : ["[title='Forms']"]
+})
